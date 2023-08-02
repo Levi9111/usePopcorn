7fc0d2d7c53b2551cfebae46f6f6e412e614bef5
Levi9111 <captainlevi9111@gmail.com>
2023-08-02 09:41:15 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Test() {
+  const [rate, setRate] = useState(0);
+  return (
+    <>
+      <StarRating color="blue" maxRating={10} onSetRating={setRate} />
+      <p>This movie has {rate} ratings</p>
+    </>
+  );
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "average", "good", "better", "excellent"]}
+    />
+    <StarRating color="#AAEEBA" size={30} />
+    <Test />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
